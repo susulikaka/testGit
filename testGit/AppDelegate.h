6a2a36af940680d0eb3098c86051219047f1f725
suping <lisuping@lukou.com>
2017-01-04 09:58:06 +0800

    Initial Commit

---
+++ testGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by Nsamper on 2017/1/4.
+//  Copyright © 2017年 Nsamper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
