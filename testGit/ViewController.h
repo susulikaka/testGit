e3bcf2d365552af18db1c88406941c309f2b421c
suping <lisuping@lukou.com>
2017-01-11 12:30:53 +0800

    new2 init with gco --orphan new2

---
+++ testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by Nsamper on 2017/1/4.
+//  Copyright © 2017年 Nsamper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testGit/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  testGit
+//
+//  Created by Nsamper on 2017/1/4.
+//  Copyright © 2017年 Nsamper. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    //before gco new
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+//modified
+//master
+//master2
+// new 1
+//noadded
+- (void)viewWillLayoutSubviews {
+    
+}
+
+@end
